5a59987db2a6230dd836c5e4b459d42422a07a57
DJ Draper <darcyjdraper@gmail.com>
2020-07-22 02:55:30 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -47,4 +47,8 @@ def add_characters
       }
    }
    
-   
+   epic_tragedy[:additional_characters] = []
+   epic_tragedy[:additional_characters][0] = { name: 'Prince Escalus' }
+   epic_tragedy[:additional_characters][1] = { name: 'Apothecary' }
+
+end
